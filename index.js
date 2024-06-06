04e4c4b5bc056b72a48cb3ff54ee6454ba7d64f2
Visheshkumar <visheshkumar95647@gmail.com>
2024-06-07 00:06:00 +0530

    a

--- index.js
+++ index.js
@@ -4,25 +4,19 @@ const app = express();
 const mongoose = require('mongoose');
 const cors = require("cors");
 const dotenv = require("dotenv");
-
 dotenv.config();
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
+
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log("Database connected");
-        app.listen(process.env.PORT || 5000, () => {
-            console.log(`Server is running on port ${process.env.PORT || 5000}`);
-        });
-    })
-    .catch(error => {
-        console.error("Error connecting to database:", error);
+mongoose.connect(process.env.DATABASE).then(() => {
+    console.log("Database connected");
+    app.listen(process.env.PORT || 5000, () => {
+        console.log('Server is running on port 5000');
     });
-
-// Import routes
+}).catch(error => {
+    console.error("Error connecting to database:", error);
+});
 app.use(require('./Routes/userroutes'));
