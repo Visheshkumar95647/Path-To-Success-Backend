d969a569f80548d101fe6c06133611060d47fa51
Visheshkumar <visheshkumar95647@gmail.com>
2024-06-06 23:30:03 +0530

    Faltu

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ const path = require("path");
 const app = express();
 const mongoose = require('mongoose');
 const cors = require("cors");
-
+const dotenv = require("dotenv");
+dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-mongoose.connect("mongodb://localhost:27017/job").then(() => {
+mongoose.connect(process.env.DATABASE).then(() => {
     console.log("Database connected");
-    app.listen(process.env.PORT , () => {
+    app.listen(process.env.PORT || 5000, () => {
         console.log('Server is running on port 5000');
     });
 }).catch(error => {
